5be01dd05715177130e98665006d541b5a6871cc
rishabh-successive <rishabh.yadav@successive.tech>
2021-01-06 14:37:32 +0530

    feat:change in login

--- src/lib/utils/api.js
+++ src/lib/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const callApi = async (route, method, body) => {
-  const BASE_URL = 'http://localhost:9000/api';
+  const BASE_URL = 'http://localhost:7000/api';
   const response = await axios({
     method,
     url: BASE_URL + route,
